f63bb3657206684594671f702d65e9afc3aec19e
motomichi <motomichi.motomichi@gmail.com>
2022-12-24 18:38:56 +0900

    .eslintrc.jsの編集

--- next_js_introduction/.eslintrc.js
+++ next_js_introduction/.eslintrc.js
@@ -1,3 +1,30 @@
 module.exports = {
-  extends: "next/core-web-vitals",
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    ],
+    "react/jsx-props-no-spreading": "off",
+  },
 };
