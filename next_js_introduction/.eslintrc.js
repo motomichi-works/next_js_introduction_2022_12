9cd0702fd7e8bedd1fc17b5651300e59aef30aba
motomichi <motomichi.motomichi@gmail.com>
2022-12-24 19:57:21 +0900

    .eslintrc.jsもVSCodeで一度開いて保存することで成形

--- next_js_introduction/.eslintrc.js
+++ next_js_introduction/.eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
   extends: [
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "next/core-web-vitals",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "prettier",
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
   ],
   parserOptions: {
-    project: "./tsconfig.eslint.json",
+    project: './tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   root: true,
   rules: {
-    "react/function-component-definition": [
+    'react/function-component-definition': [
       2,
-      { namedComponents: "arrow-function" },
+      { namedComponents: 'arrow-function' },
     ],
-    "react/jsx-filename-extension": [
-      "error",
+    'react/jsx-filename-extension': [
+      'error',
       {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     ],
-    "react/jsx-props-no-spreading": "off",
+    'react/jsx-props-no-spreading': 'off',
   },
 };
